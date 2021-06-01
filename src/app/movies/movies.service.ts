a63e4f96a424ab1e76d06cb341109f2adaa439d5
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-01 14:01:19 -0300

    Implements apy key, base url and getMovies method

--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { MoviesModule } from './movies.module';
 
 @Injectable({
   providedIn: MoviesModule,
 })
 export class MoviesService {
-  constructor() {}
+  private API_KEY = 'd416af5d4faee64e25ab001d87aab5c3';
+  private BASE_URL = 'https://api.themoviedb.org/3';
+  constructor(private http: HttpClient) {}
+  getMovies(): Observable<any> {
+    return this.http.get(
+      `${this.BASE_URL}/movie/latest?api_key=${this.API_KEY}`
+    );
+  }
 }
