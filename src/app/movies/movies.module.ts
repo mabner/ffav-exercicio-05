da9ab506e65db9868e8a4fbaa4ea4a669c5c7fd1
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-01 14:05:42 -0300

    movieItem base files

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    MovieListComponent
+    MovieListComponent,
+    MovieItemComponent
   ],
   imports: [CommonModule, HttpClientModule],
 })
