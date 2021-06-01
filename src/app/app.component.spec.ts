97f9d818a7791095e97f2b9dea2432c69283347c
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-01 13:42:10 -0300

    Corrige pra exercicio05

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'exercicio04'`, () => {
+  it(`should have as title 'exercicio05'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('exercicio04');
+    expect(app.title).toEqual('exercicio05');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('exercicio04 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'exercicio05 app is running!'
+    );
   });
 });
